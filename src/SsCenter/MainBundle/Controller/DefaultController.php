669ee03addeee34ee680b09a8086ccb628030087
Luis Carlos Torres <ing.iuqol@gmail.com>
2015-07-02 15:47:05 -0500

    Cambios a Routing

--- src/SsCenter/MainBundle/Controller/DefaultController.php
+++ src/SsCenter/MainBundle/Controller/DefaultController.php
@@ -3,11 +3,29 @@
 namespace SsCenter\MainBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use \Symfony\Component\HttpFoundation\Response;
+use \Symfony\Component\HttpFoundation\Request;
 
 class DefaultController extends Controller
 {
-    public function indexAction($name)
+    /**
+     * 
+     * @return type
+     */
+    public function mainAction()
     {
-        return $this->render('MainBundle:Default:index.html.twig', array('name' => $name));
+        return new Response($this->displayMain());
+    }
+    
+    
+    public function displayMain()
+    {
+      $temp = $this->get('templating');
+      $result = false;
+      return $temp->render('MainBundle:Default:index.html.twig',  array
+              ('prueba' => 'VALOR1',
+               'usuario' => array ('name' => 'Luis', 'doc' => 'cc'),
+               'res' => $result,
+          )      );
     }
 }
